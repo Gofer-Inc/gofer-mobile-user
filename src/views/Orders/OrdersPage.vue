d283ca9a50aa8cb30955e836fb66ad33602a696d
whytepeter <emmanuelwhytepeter@gmail.com>
2023-05-06 16:55:28 +0100

    working on order page

--- src/views/Orders/OrdersPage.vue
+++ src/views/Orders/OrdersPage.vue
@@ -2,7 +2,29 @@
   <ion-page>
     <gNav title="Orders" />
     <ion-content fullscreen>
-      <ExploreContainer name="Orders" />
+      <div class="flex flex-col gap-6 ion-padding">
+        <div>
+          <gTitle title="Upcoming Orders" />
+          <div class="flex flex-col gap-6 mt-4">
+            <PackageCard
+              v-for="item in packages"
+              :key="item._id"
+              :data="item"
+            />
+          </div>
+        </div>
+        <div>
+          <gTitle title="Pasted Orders" />
+          <div class="flex flex-col gap-6 mt-4">
+            <PackageCard
+              class="opacity-50"
+              v-for="item in packages"
+              :key="item._id"
+              :data="item"
+            />
+          </div>
+        </div>
+      </div>
     </ion-content>
   </ion-page>
 </template>
@@ -10,5 +32,12 @@
 <script setup>
 import { IonPage, IonContent } from "@ionic/vue";
 // import { chevronDown, filter, notifications } from "ionicons/icons";
-import ExploreContainer from "@/components/ExploreContainer.vue";
+
+import PackageCard from "@/components/Delivery/PackageCard.vue";
+import { computed } from "vue";
+
+import packagesDB from "@/utils/packages.js";
+
+const packages = computed(() => packagesDB);
+console.log(packages.value);
 </script>
